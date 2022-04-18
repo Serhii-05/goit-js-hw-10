cb5b5e6689e2a638b6a9b95d131dadb5962ae347
Serhii Melnychenko <higankingpro@gmail.com>
2022-04-18 15:38:16 +0300

    delit code

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import fetchCountri from './fetchCountri';
+
+import fetchCountries from './fetchCountri';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 
 const DEBOUNCE_DELAY = 300;
 
@@ -11,47 +11,47 @@ const countryInfo = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
-
 function onInputChange() {
-    const isFilled = input.value.trim();
-    countryList.innerHTML = '';
-    countryInfo.innerHTML = '';
-    if (isFilled) {
-        fetchCountri(isFilled)
-            .then(dataProcessing)
-            .catch(error => {
-                Notify.failure('Oops, there is no country with that name');
-                console.log(error);
-            });
+  const isFilled = input.value.trim();
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  if (isFilled) {
+    fetchCountries(isFilled)
+      .then(dataProcessing)
+      .catch(error => {
+        Notify.failure('Oops, there is no country with that name');
+        console.log(error);
+      });
+  }
+
+  function dataProcessing(data) {
+    if (data.length > 10) {
+      Notify.info('Too many matches found. Please enter a more specific name.');
+
+      return;
     }
 
-    function dataProcessing(data) {
-        if (data.leng > 10) {
-            Notify.info('Too many matches found. Please enter a more specific name.');
+    markup(data);
+  }
 
-            return;
-        }
-        markup(data);
-    }
+  function markup(data) {
+    const markupData = data
+      .map(({ flags: { svg }, name: { official } }) => {
+        return `<li><img src="${svg}" alt="${official}" width="100" height="50"/>${official}</li>`;
+      })
+      .join('');
 
+    if (data.length === 1) {
+      const languages = Object.values(data[0].languages).join(', ');
 
-    function markup(data) {
-        const markupData = data.map(({ flag: { svg }, name: { official } }) => {
-            return `<li><img src="${svg}" alt="${official}" width="100" height="50"/>${official}</li>`;
-        })
-            .join('');
-        
-        if (data.length === 1) {
-            const languages = Object.values(data[0].languages).join(', ');
-
-            const markupInfo = `<ul>
+      const markupInfo = `<ul>
       <li>Capital: ${data[0].capital}</li>
       <li>Population: ${data[0].population}</li>
       <li>Languages: ${languages}</li>
       </ul>`;
-            countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
-        }
 
-        return countryList.insertAdjacentHTML('afterbegin', markupData);
+      countryInfo.insertAdjacentHTML('afterbegin', markupInfo);
     }
+    return countryList.insertAdjacentHTML('afterbegin', markupData);
+  }
 }
